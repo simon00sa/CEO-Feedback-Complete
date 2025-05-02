3b0b2b961ed854f2b1a78b72e983f445f120f091
simon00sa <simon@imaginality.co.za>
2025-05-02 16:51:16 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
-import { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ['@prisma/client', '@auth/prisma-adapter'],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      async_hooks: false,
+      child_process: false,
+      events: false,
+    };
+    return config;
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
