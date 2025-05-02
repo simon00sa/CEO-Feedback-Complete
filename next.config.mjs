bec4b468d3e31a6828bb3a7dfb82f94a933a2241
simon00sa <simon@imaginality.co.za>
2025-05-02 16:21:39 +0200

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', '@auth/prisma-adapter'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      async_hooks: false,
+      child_process: false,
+      events: false,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
