8075598dac29218f7dd2ab5eede4a99b8f4c5b3a
simon00sa <simon@imaginality.co.za>
2025-05-02 17:04:34 +0200

    Update route.ts

--- src/app/api/admin/invitations/route.ts
+++ src/app/api/admin/invitations/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from '@/lib/authOptions';
 import { addHours } from 'date-fns'; // Using date-fns for date manipulation
 
 const prisma = new PrismaClient();
