990f4ecafbcbbfdf2661019b0213e5900e50997c
simon00sa <simon@imaginality.co.za>
2025-05-04 12:13:06 +0200

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({
+    log: process.env.NODE_ENV === 'development' 
+      ? ['query', 'error', 'warn'] 
+      : ['error']
+  });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
