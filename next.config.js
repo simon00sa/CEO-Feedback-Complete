09461084b1403c26838e5708e40c11bbd70e973f
simon00sa <simon@imaginality.co.za>
2025-05-08 14:13:37 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Handle external dependencies for server-side only
+      config.externals = [
+        ...config.externals,
+        "nodemailer", // Ensure nodemailer is treated as an external dependency
+      ];
+    }
+
     // Alias node-prefixed core modules to their plain counterparts
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -13,13 +21,6 @@ const nextConfig = {
       "node:child_process": "child_process",
       "node:module": "module",
     };
-
-    if (isServer) {
-      config.externals = {
-        ...config.externals,
-        "nodemailer": "commonjs nodemailer",
-      };
-    }
 
     return config;
   },
