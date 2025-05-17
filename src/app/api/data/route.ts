5c681e0de15130ab020998f286ef3c167d0738aa
simon00sa <simon@imaginality.co.za>
2025-05-17 19:13:26 +0200

    Update route.ts

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma"; // Use the Prisma Client from the shared library
+import prisma from "@/lib/prisma"; // Prisma Client instance
 
 // Handler for GET requests
 export async function GET(request: Request) {
